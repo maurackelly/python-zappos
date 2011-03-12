20278e99f8f26d16bbb7449b5616a7a1bdf29e32
Maura Chace <minarets@gmail.com>
2011-03-12 17:40:10 -0500

    Added basic error handling.

--- zappos.py
+++ zappos.py
@@ -27,14 +27,20 @@ class ZapposApi(object):
         data['key'] = self.api_key
         url = '%s%s?%s' % (self.BASE_URL, path, urllib.urlencode(data))
         response = urllib.urlopen(url)
-        return json.load(response)
+        try:
+            result = json.load(response)
+        except ValueError:
+            raise ZapposApiException, "Did not receive a JSON response."
+        if result['statusCode'] != '200':
+            raise ZapposApiException, "Status code %s" % result['statusCode']
+        return result
 
     def _camel_case(self, value):
         """ For the awesome urls """
         return string.capwords(value.replace('_', ' ')).replace(' ','')
 
     def __getattr__(self, name):
-        """ Voodoo """
+        """ Magic getattr. I welcome better syntax suggestions. """
         method = self._camel_case(name)
 
         def handler(**kwargs):
@@ -47,8 +53,9 @@ class ZapposApi(object):
 def main():
     from config import ZAPPOS_KEY
     api = ZapposApi(ZAPPOS_KEY)
-    print api.product(id=7718435)
-    print api.brand(id=1425)
+    print api.product(id=7718435, includes='["gender","styles","stocks"]')
+    # print api.brand(id=1425)
+    # print api.similarity(type="moreLikeThis", style_id=7718435)
 
 
 if __name__ == '__main__':
